9f693903fbd737abca95b38ef2bed8f6d906f2a9
lucqiiiii <qi_guo@umail.ucsb.edu>
2018-12-04 17:01:51 -0800

    hard

--- word.h
+++ word.h
@@ -1,4 +1,4 @@
-//Qi Guo Project1
+//Qi Guo Project2
 #ifndef WORD_H
 #define WORD_H
 #include <iostream>
@@ -53,6 +53,49 @@ public:
 };
 
 //Helper Methods:
+
+word* list_search(word &head, const word::value_type &w){
+    word *cursor = head;
+    if(cursor == NULL){
+        return NULL;
+    }
+    else{
+        while((cursor != NULL) && (cursor -> word() != w)){
+            cursor = cursor -> next();
+        }
+        return cursor;
+    }
+}
+
+void insert_word(word *&head, word *prev, const word::value_type &w){
+    if(prev == NULL){
+        word *temp;
+        temp = new word(w);
+        head = temp;
+    }
+    else{
+        cursor = list_search(head,w);
+        if(cursor != NULL){
+            update_list(cursor);
+        }
+        else{
+            word *temp;
+            temp = new word(w,prev -> next(),prev);
+            prev -> set_next(temp);
+            temp -> next() -> set_prev(temp);
+            update_list(temp);
+        }
+    }
+}
+
+void update_list(const word *w){
+
+
+
+}
+
+
+
 
 
 
